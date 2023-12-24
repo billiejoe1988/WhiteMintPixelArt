389a2d09fee38867a5110554d50bd15ea2e18ead
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2023-12-24 01:31:50 -0300

    fix buildd

--- src/components/CheckOutForm/CheckOutForm.jsx
+++ src/components/CheckOutForm/CheckOutForm.jsx
@@ -1,4 +1,4 @@
-import './CheckOutForm.css';
+import './checkOutForm.css';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
